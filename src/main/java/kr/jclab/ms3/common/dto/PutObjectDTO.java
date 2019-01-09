8bf9efc6030e1322dbbc81851ed90ab47e930ee7
Jichan <development@jc-lab.net>
2019-01-09 16:36:38 +0900

    Support backetList and putObject

---
+++ src/main/java/kr/jclab/ms3/common/dto/PutObjectDTO.java
@@ -0,0 +1,10 @@
+package kr.jclab.ms3.common.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class PutObjectDTO {
+    public static class Response {
+        public int code;
+        public String message;
+    }
+}
